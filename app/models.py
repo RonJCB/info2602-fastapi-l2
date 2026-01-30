2234e126829340163c1684bef27d522b4b9b969f
RonJCB <ronellbaptiste30@gmail.com>
2026-01-29 20:36:36 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,17 @@ class User(SQLModel, table=True):
     username:str = Field(index=True, unique=True)
     email:str = Field(index=True, unique=True)
     password:str
+#added 3 methods to the user class 
+#first one makes a intances of the class
+#second one 
+#third one prints the instance to the console
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.set_password(password)
+
+    def set_password(self, password):
+        self.password = password_hash.hash(password)
+
+    def __str__(self) ->str:
+        return f"(User id = {self.id}, username = {self.username}, email = {self.email})"
